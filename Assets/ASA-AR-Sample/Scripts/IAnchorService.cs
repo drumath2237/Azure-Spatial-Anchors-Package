a3045d85e98062b98d59ea1520de744d5740572b
drumath <2237k.drumath@gmail.com>
2022-08-22 03:44:33 +0900

    :sparkles: anchorInfoを追加

---
+++ Assets/ASA-AR-Sample/Scripts/IAnchorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+[Serializable]
+public struct AnchorInfo
+{
+    /// <summary>
+    /// 空間アンカーの識別子
+    /// </summary>
+    public string anchorKey;
+
+    /// <summary>
+    /// 破棄される時間を文字列化にシリアライズしたもの
+    /// dateTime.toString()で生成される想定
+    /// </summary>
+    public string expireOn;
+}
+
+public interface IAnchorService
+{
+}
