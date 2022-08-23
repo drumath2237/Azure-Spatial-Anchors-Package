aa996353bc79d99d9cdab1c35803b3b8078d5c1f
drumath <2237k.drumath@gmail.com>
2022-08-24 03:32:11 +0900

    anchor finderの実装

---
+++ Assets/ASA-AR-Sample/Scripts/AnchorFinder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.SpatialAnchors;
+using Microsoft.Azure.SpatialAnchors.Unity;
+
+public class AnchorFinder
+{
+    private readonly SpatialAnchorManager _spatialAnchorManager;
+    private readonly IAnchorService _anchorService;
+
+    public event Action<CloudNativeAnchor> OnAnchorFound = null;
+
+    private CloudNativeAnchor _nativeAnchor = null;
+
+    public AnchorFinder(SpatialAnchorManager spatialAnchorManager, IAnchorService anchorService)
+    {
+        _spatialAnchorManager = spatialAnchorManager;
+        _anchorService = anchorService;
+    }
+
+    public Task StartSession()
+    {
+        _spatialAnchorManager.AnchorLocated += UpdateNativeAnchor_OnAnchorFound;
+        return _spatialAnchorManager.StartSessionAsync();
+    }
+
+    public void DestroySession()
+    {
+        _spatialAnchorManager.AnchorLocated -= UpdateNativeAnchor_OnAnchorFound;
+        _spatialAnchorManager.DestroySession();
+    }
+
+    private void UpdateNativeAnchor_OnAnchorFound(object sender, AnchorLocatedEventArgs args)
+    {
+        if (args.Status != LocateAnchorStatus.Located)
+        {
+            return;
+        }
+
+        if (_nativeAnchor is null)
+        {
+            return;
+        }
+
+        _nativeAnchor.CloudToNative(args.Anchor);
+        OnAnchorFound?.Invoke(_nativeAnchor);
+    }
+
+    public async Task FindAnchor(CloudNativeAnchor nativeAnchor)
+    {
+        var anchorKey = await _anchorService.TryGetLatestAnchor();
+        if (anchorKey is null)
+        {
+            return;
+        }
+
+        _nativeAnchor = nativeAnchor;
+        var anchorCriteria = new AnchorLocateCriteria
+        {
+            Identifiers = new[] { anchorKey.Value.anchorKey }
+        };
+        _spatialAnchorManager.Session.CreateWatcher(anchorCriteria);
+    }
+}
