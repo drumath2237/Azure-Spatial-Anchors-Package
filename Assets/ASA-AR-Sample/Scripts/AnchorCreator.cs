d8cc9cc6ac8b42724bcc48ca9d308bf74a6a6f71
drumath <2237k.drumath@gmail.com>
2022-08-24 02:51:25 +0900

    anchorCreatorの実装

---
+++ Assets/ASA-AR-Sample/Scripts/AnchorCreator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.SpatialAnchors;
+using Microsoft.Azure.SpatialAnchors.Unity;
+using UnityEngine;
+
+public class AnchorCreator
+{
+    private readonly SpatialAnchorManager _spatialAnchorManager;
+    private readonly IAnchorService _anchorService;
+
+    public AnchorCreator(SpatialAnchorManager anchorManager, IAnchorService anchorService)
+    {
+        _spatialAnchorManager = anchorManager;
+        _anchorService = anchorService;
+    }
+
+    public Task StartSession() => _spatialAnchorManager.StartSessionAsync();
+
+    public async Task<CloudSpatialAnchor> CreateAnchor(CloudNativeAnchor nativeAnchor, int anchorExpirationDays = 1)
+    {
+        if (!_spatialAnchorManager.IsReadyForCreate)
+        {
+            return null;
+        }
+
+        await nativeAnchor.NativeToCloud(false);
+
+        var cloudSpatialAnchor = nativeAnchor.CloudAnchor;
+        cloudSpatialAnchor.Expiration = DateTimeOffset.Now.AddDays(anchorExpirationDays);
+
+        try
+        {
+            await _spatialAnchorManager.CreateAnchorAsync(cloudSpatialAnchor);
+            if (cloudSpatialAnchor.Identifier == string.Empty)
+            {
+                return null;
+            }
+
+            await _anchorService.CreateAnchor(new AnchorInfo(
+                cloudSpatialAnchor.Identifier,
+                cloudSpatialAnchor.Expiration.DateTime
+            ));
+
+            return cloudSpatialAnchor;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            throw;
+        }
+    }
+}
--- Assets/ASA-AR-Sample/Scripts/IAnchorService.cs
+++ Assets/ASA-AR-Sample/Scripts/IAnchorService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Threading.Tasks;
 
 [Serializable]
@@ -14,6 +15,12 @@ public struct AnchorInfo
     /// dateTime.toString()で生成される想定
     /// </summary>
     public string expireOn;
+
+    public AnchorInfo(string key, DateTime expirationDate)
+    {
+        anchorKey = key;
+        expireOn = expirationDate.ToString(CultureInfo.CurrentCulture);
+    }
 }
 
 public interface IAnchorService
