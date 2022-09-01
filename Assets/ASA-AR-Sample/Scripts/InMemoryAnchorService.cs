296e22c9fa66f16f7bee36d750d519edcc85373e
drumath <2237k.drumath@gmail.com>
2022-09-01 19:21:59 +0900

    :sparkles: in memory anchor serviceの実装

---
+++ Assets/ASA-AR-Sample/Scripts/InMemoryAnchorService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+public class InMemoryAnchorService : IAnchorService
+{
+    private AnchorInfo? _latestAnchorInfo;
+
+    public InMemoryAnchorService()
+    {
+        _latestAnchorInfo = null;
+    }
+    public Task CreateAnchor(AnchorInfo anchorInfo)
+    {
+        _latestAnchorInfo = anchorInfo;
+        return Task.CompletedTask;
+    }
+
+    public Task<AnchorInfo?> TryGetLatestAnchor()
+    {
+        return Task.FromResult(_latestAnchorInfo);
+    }
+}
