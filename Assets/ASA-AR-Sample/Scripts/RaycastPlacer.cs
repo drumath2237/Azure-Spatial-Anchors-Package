62b3b7454fc28c51ecd283a4b3503168320872ef
drumath <2237k.drumath@gmail.com>
2022-09-01 19:39:00 +0900

    :sparkles: raycastがUIを貫通しなくなった

--- Assets/ASA-AR-Sample/Scripts/RaycastPlacer.cs
+++ Assets/ASA-AR-Sample/Scripts/RaycastPlacer.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 public class RaycastPlacer : MonoBehaviour
 {
     [SerializeField] private ARRaycastManager arRaycastManager;
-    
+
     [SerializeField] private Transform placeObjectTransform;
 
     private List<ARRaycastHit> _hits = new();
@@ -23,15 +24,31 @@ public class RaycastPlacer : MonoBehaviour
         {
             return;
         }
+
         var touchPosition = Input.GetTouch(0).position;
 
-        if (!arRaycastManager.Raycast(touchPosition, _hits))
+        if (IsPointOverUIObject(touchPosition) || !arRaycastManager.Raycast(touchPosition, _hits))
         {
             return;
         }
-        
+
         placeObjectTransform.position = _hits[0].pose.position;
         placeObjectTransform.rotation = _hits[0].pose.rotation;
+    }
 
+    private static bool IsPointOverUIObject(Vector2 pos)
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return false;
+        }
+        
+        var eventDataCurrentPosition = new PointerEventData(EventSystem.current)
+        {
+            position = new Vector2(pos.x, pos.y)
+        };
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
