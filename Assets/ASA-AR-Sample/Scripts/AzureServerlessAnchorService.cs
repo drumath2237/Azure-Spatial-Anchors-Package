8f4552e9db033333c84b1f7ff28dd18d6a784056
drumath <2237k.drumath@gmail.com>
2022-09-05 19:34:09 +0900

    :sparkles: baseUrlの設定をコンストラクタに移動

--- Assets/ASA-AR-Sample/Scripts/AzureServerlessAnchorService.cs
+++ Assets/ASA-AR-Sample/Scripts/AzureServerlessAnchorService.cs
@@ -2,6 +2,13 @@
 
 public class AzureServerlessAnchorService : IAnchorService
 {
+    readonly string baseUrl;
+
+    public AzureServerlessAnchorService(string apiBaseUrl = "https://shoten13-anchorinfo.azurewebsites.net")
+    {
+        baseUrl = apiBaseUrl;
+    }
+
     public Task CreateAnchor(AnchorInfo anchorInfo)
     {
         throw new System.NotImplementedException();
